2b6a00c9bb26f64cfb33e795aefb73b10bdae391
dygames <dygames@naver.com>
2016-11-16 23:47:14 +0900

    Create Project

---
+++ Assets/Script/Gaze.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gaze : MonoBehaviour
+{
+
+    MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponentInChildren<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        Vector3 headposition = Camera.main.transform.position;
+        Vector3 gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitinfo;
+        if (Physics.Raycast(headposition, gazeDirection, out hitinfo))
+        {
+            transform.position = hitinfo.point;
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, hitinfo.normal);
+            meshRenderer.enabled = true;
+        }
+        else
+        {
+            meshRenderer.enabled = false;
+        }
+
+    }
+}
